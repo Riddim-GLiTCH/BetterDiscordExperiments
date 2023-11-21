5f5a35bee5da6fa22f25c0fbe8eac91a05be1d7b
Skye <Riddim_GLiTCH@courvix.com>
2023-11-21 09:29:35 +0100

    Update BetterDiscordExperiments.plugin.js
    Replace plugin with rewrite by Zrodevkaan

--- BetterDiscordExperiments.plugin.js
+++ BetterDiscordExperiments.plugin.js
@@ -1,44 +1,27 @@
-/**
+
  * @name BetterDiscordExperiments
  * @description Enables the experiments tab in discord's settings.
- * @author Riddim_GLiTCH
- * @version 1.0.0
+ * @author originally by Riddim_GLITCH, Rewritten from the ground up by Zrodevkaan
+ * @version 1.0.1
 */
+class ExperimentsPlugin
+{
+    start = () => {
+        let c = webpackChunkdiscord_app.push([[Symbol()],{},({c})=>Object.values(c)]);
+        let u = c.find((x)=> x?.exports?.default?.getUsers).exports.default;
+        let m = Object.values(u._dispatcher._actionHandlers._dependencyGraph.nodes);
+    
+        u.getCurrentUser().flags |= 1;
+        m.find((x)=>x.name === "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]();
+        try {m.find((x)=>x.name === "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user:{flags: 1}})} catch {}
+        m.find((x)=>x.name === "ExperimentStore").storeDidChange()
+    }
 
-let mod;
-let usermod;
-let nodes;
-let oldGetUser;
-class MyPlugin {
-  constructor() {
-  }
+    stop()
+    {
 
-  start() {
-let wpRequire;
-window.webpackChunkdiscord_app.push([[ Math.random() ], {}, (req) => { wpRequire = req; }]);
-mod = Object.values(wpRequire.c).find(x => typeof x?.exports?.Z?.isDeveloper !== "undefined");
-usermod = Object.values(wpRequire.c).find(x => x?.exports?.default?.getUsers)
-nodes = Object.values(mod.exports.Z._dispatcher._actionHandlers._dependencyGraph.nodes)
-try {
-    nodes.find(x => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user: {flags: 1}})
-} catch (e) {}
-oldGetUser = usermod.exports.default.__proto__.getCurrentUser;
-usermod.exports.default.__proto__.getCurrentUser = () => ({isStaff: () => true})
-nodes.find(x => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]()
-usermod.exports.default.__proto__.getCurrentUser = oldGetUser
-  }
-
-  stop() {
-    // Perform necessary cleanup or tear down of resources when the plugin is stopped
-  }
-
-  onLoad() {
-    this.start();
-  }
-
-  onUnload() {
-    this.stop();
-  }
+    }
+    
 }
 
-module.exports = MyPlugin;
+module.exports = ExperimentsPlugin
